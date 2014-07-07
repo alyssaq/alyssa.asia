94110eedcb1171bca7108141464f4e66decd36d4
Alyssa <alyssaquek@gmail.com>
2014-07-07 16:10:16 +0800

    pep8 formatting

--- webserver.py
+++ webserver.py
@@ -5,7 +5,9 @@ import webapp2
 HTML_PATH = "templates"
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), HTML_PATH)),
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), HTML_PATH)
+    ),
     extensions=['jinja2.ext.autoescape'])
 
 class MainPage(webapp2.RequestHandler):
@@ -16,7 +18,7 @@ class MainPage(webapp2.RequestHandler):
         filename = base_href if bool(base_href) else "index"
         try:
             template = JINJA_ENVIRONMENT.get_template("%s.html" % filename)
-        except jinja2.TemplateNotFound:    
+        except jinja2.TemplateNotFound:
             template = JINJA_ENVIRONMENT.get_template("404.html")
 
         self.response.write(template.render(template_values))
